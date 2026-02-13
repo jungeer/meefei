361491d84aad44738fa22e96bc356afa02b34830
zengjiajun <zengjiajun@zszhizhu.com>
2026-02-13 22:55:18 +0800

    fix: update AnniversaryCounter to include "至今" in the date display for clarity

--- src/components/AnniversaryCounter.vue
+++ src/components/AnniversaryCounter.vue
@@ -8,7 +8,7 @@ const { days } = useAnniversary();
 <template>
   <div class="anniversary-counter">
     <p class="anniversary-counter__label">我们在一起</p>
-    <p class="anniversary-counter__date">始于 {{ START_DATE_DISPLAY }}</p>
+    <p class="anniversary-counter__date">始于 {{ START_DATE_DISPLAY }} 至今</p>
     <p class="anniversary-counter__days">
       第 <span class="anniversary-counter__number">{{ days }}</span> 天啦
     </p>
